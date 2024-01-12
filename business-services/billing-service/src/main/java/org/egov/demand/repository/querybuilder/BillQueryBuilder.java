da2686fb02e8192eff950ad9fa0624024d849acc
Amitsinghongraph <135822664+Amitsinghongraph@users.noreply.github.com>
2024-01-12 15:19:22 +0530

    added missing payid, consumercode

--- business-services/billing-service/src/main/java/org/egov/demand/repository/querybuilder/BillQueryBuilder.java
+++ business-services/billing-service/src/main/java/org/egov/demand/repository/querybuilder/BillQueryBuilder.java
@@ -22,8 +22,8 @@ public class BillQueryBuilder {
 	public static final String BILL_STATUS_UPDATE_BASE_QUERY = "UPDATE egbs_bill_v1 SET status=? {replace} WHERE status='ACTIVE' AND tenantId = ? ";
 	
 	public static final String INSERT_BILL_QUERY = "INSERT into egbs_bill_v1 "
-			+"(id, tenantid, payername, payeraddress, payeremail, isactive, iscancelled, createdby, createddate, lastmodifiedby, lastmodifieddate, mobilenumber, status, additionaldetails)"
-			+"values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			+"(id, tenantid, payername, payeraddress, payeremail, isactive, iscancelled, createdby, createddate, lastmodifiedby, lastmodifieddate, mobilenumber, status, additionaldetails, payerid, consumercode)"
+			+"values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 	
 	public static final String INSERT_BILLDETAILS_QUERY = "INSERT into egbs_billdetail_v1 "
 			+"(id, tenantid, billid, demandid, fromperiod, toperiod, businessservice, billno, billdate, consumercode, consumertype, billdescription, displaymessage, "
